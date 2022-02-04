73804616fca19afffce2ef42fe99e930a20521c0
sieva-cmd <musyokasieva@gmail.com>
2022-02-04 21:02:24 +0300

    add like and dislike button

--- src/app/display-qoute/display-qoute.component.ts
+++ src/app/display-qoute/display-qoute.component.ts
@@ -9,8 +9,18 @@ import { Quotes } from '../quotes';
 export class DisplayQouteComponent implements OnInit {
   quotes:Quotes[] =[
     {id:1, name:'Sieva Lucia', lists:'Every pain gives a lesson and every lesson changes a person',author:'Martin Luther', upvote:'',downvote:''},
-    {id:2, name:'Brian Githinji',lists:'Common sense is not a gift, it`s a punishment because you have to deal with everyone who deesnt have it', author:'Nelson Madela',upvote:'',downvote:''},
+    {id:2, name:'Brian Githinji',lists:'Common sense is not a gift, it`s a punishment because you have to deal with everyone who deesnt have it', author:'Nelson Madela',upvote:'',downvote:' '},
   ]
+
+  numberOfLikes :number = 1;
+  numberOfDislikes :number =0;
+
+  likeButtonClick(){
+this.numberOfLikes++;
+  }
+  dislikeButtonClick(){
+this.numberOfDislikes--;
+  }
 
 
   constructor() { }
